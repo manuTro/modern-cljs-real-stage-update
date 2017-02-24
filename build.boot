a0b21f43ea37cc5d8579857b2cd3d9bd861a7011
Manuela <manuela@Ubuntu.station>
2017-02-24 14:43:34 +0100

    starting with update

--- build.boot
+++ build.boot
@@ -1,22 +1,22 @@
 (set-env!
  :source-paths #{"src/clj" "src/cljs"}
  :resource-paths #{"html"}
- 
+
  :dependencies '[
-                 [org.clojure/clojure "1.7.0"]         ;; add CLJ
-                 [org.clojure/clojurescript "1.7.170"] ;; add CLJS
+                 [org.clojure/clojure "1.8.0"]         ;; add CLJ
+                 [org.clojure/clojurescript "1.9.473"] ;; add CLJS
                  [adzerk/boot-cljs "1.7.170-3"]
-                 [pandeiro/boot-http "0.7.0"]
-                 [adzerk/boot-reload "0.4.2"]
-                 [adzerk/boot-cljs-repl "0.3.0"]       ;; add bREPL
-                 [com.cemerick/piggieback "0.2.1"]     ;; needed by bREPL 
+                 [pandeiro/boot-http "0.7.6"]
+                 [adzerk/boot-reload "0.5.1"]
+                 [adzerk/boot-cljs-repl "0.3.0"]      ;; add bREPL
+                 [com.cemerick/piggieback "0.2.1"]     ;; needed by bREPL
                  [weasel "0.7.0"]                      ;; needed by bREPL
                  [org.clojure/tools.nrepl "0.2.12"]    ;; needed by bREPL
                  [org.clojars.magomimmo/domina "2.0.0-SNAPSHOT"]
                  [hiccups "0.3.0"]
-                 [compojure "1.4.0"]                   ;; for routing
-                 [org.clojars.magomimmo/shoreleave-remote-ring "0.3.1"]
-                 [org.clojars.magomimmo/shoreleave-remote "0.3.1"]
+                 ;[compojure "1.4.0"]                   ;; for routing
+                ;  [org.clojars.magomimmo/shoreleave-remote-ring "0.3.1"]
+                ;  [org.clojars.magomimmo/shoreleave-remote "0.3.1"]
                  [javax.servlet/servlet-api "2.5"]
                  ])
 
@@ -26,7 +26,7 @@
          '[adzerk.boot-cljs-repl :refer [cljs-repl start-repl]])
 
 ;;; add dev task
-(deftask dev 
+(deftask dev
   "Launch immediate feedback dev environment"
   []
   (comp
--- src/cljs/modern_cljs/shopping.cljs
+++ src/cljs/modern_cljs/shopping.cljs
@@ -1,39 +1,49 @@
 (ns modern-cljs.shopping
-  (:require [domina.core :refer [append! 
+  (:require [domina.core :refer [append!
                                  by-class
-                                 by-id 
-                                 destroy! 
-                                 set-value! 
+                                 by-id
+                                 destroy!
+                                 set-value!
                                  value]]
             [domina.events :refer [listen!]]
             [hiccups.runtime]
-            [shoreleave.remotes.http-rpc :refer [remote-callback]]
+            ; [shoreleave.remotes.http-rpc :refer [remote-callback]]
             [cljs.reader :refer [read-string]])
   (:require-macros [hiccups.core :refer [html]]
-                   [shoreleave.remotes.macros :as macros]))
+                  ;  [shoreleave.remotes.macros :as macros]
+                   ))
+
+; (defn calculate []
+;   (let [quantity (read-string (value (by-id "quantity")))
+;         price (read-string (value (by-id "price")))
+;         tax (read-string (value (by-id "tax")))
+;         discount (read-string (value (by-id "discount")))]
+;     (remote-callback :calculate
+;                      [quantity price tax discount]
+;                      #(set-value! (by-id "total") (.toFixed % 2)))))
 
 (defn calculate []
-  (let [quantity (read-string (value (by-id "quantity")))
-        price (read-string (value (by-id "price")))
-        tax (read-string (value (by-id "tax")))
-        discount (read-string (value (by-id "discount")))]
-    (remote-callback :calculate
-                     [quantity price tax discount]
-                     #(set-value! (by-id "total") (.toFixed % 2)))))
+  (let [quantity (value (by-id "quantity"))
+        price (value (by-id "price"))
+        tax (value (by-id "tax"))
+        discount (value (by-id "discount"))]
+    (set-value! (by-id "total") (-> (* quantity price)
+                                    (* (+ 1 (/ tax 100)))
+                                    (- discount)
+                                    (.toFixed 2)))))
 
 (defn ^:export init []
   (when (and js/document
              (aget js/document "getElementById"))
-    (listen! (by-id "calc") 
-             :click 
+    (listen! (by-id "calc")
+             :click
              calculate)
-    (listen! (by-id "calc") 
-             :mouseover 
+    (listen! (by-id "calc")
+             :mouseover
              (fn []
                (append! (by-id "shoppingForm")
                         (html [:div.help "Click to calculate"]))))  ;; hiccups
-    (listen! (by-id "calc") 
-             :mouseout 
+    (listen! (by-id "calc")
+             :mouseout
              (fn []
                (destroy! (by-class "help"))))))
-
